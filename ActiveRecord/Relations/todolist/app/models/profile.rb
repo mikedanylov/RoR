c3406f1c1d578b382796247b4a24f8e9e19e5806
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-09 12:40:31 +0200

    Added validation to User and Profile models

--- ActiveRecord/Relations/todolist/app/models/profile.rb
+++ ActiveRecord/Relations/todolist/app/models/profile.rb
@@ -1,3 +1,20 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :first_or_last_name_exists
+  validates :gender, format: {with: /(fe)?male/}
+  validate :no_Sue_males
+
+  def first_or_last_name_exists
+    if !first_name && !last_name
+      errors.add(:first_name, "First or Last Name should be specified!");
+      errors.add(:last_name, "First or Last Name should be specified!");
+    end
+  end
+
+  def no_Sue_males
+    if first_name && first_name == "Sue"
+      errors.add(:first_name, "First Name for male can't be Sue!");
+    end
+  end
 end
