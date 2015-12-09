b3d2f100f4963ed01c4678d4a5194276b2dda75c
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-09 14:32:32 +0200

    Add a class method get_all_profiles to the Profile class

--- ActiveRecord/Relations/todolist/app/models/profile.rb
+++ ActiveRecord/Relations/todolist/app/models/profile.rb
@@ -17,4 +17,8 @@ class Profile < ActiveRecord::Base
       errors.add(:first_name, "First Name for male can't be Sue!");
     end
   end
+
+  def self.get_all_profiles(min, max)
+    Profile.where("birth_year BETWEEN :min_age and :max_age", {min_age: min, max_age: max}).order(:birth_year)
+  end
 end
