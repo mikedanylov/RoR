ba5b4fec0ee3f084f04ecd95390589fa2bcde27a
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-17 22:09:28 +0200

    Added nested resource todo_items

--- ActiveRecord/NestedResources-Security-Paging/todolist/config/routes.rb
+++ ActiveRecord/NestedResources-Security-Paging/todolist/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root to: "todo_lists#index"
 
-  resources :todo_lists
+  resources :todo_lists do
+    resources :todo_items
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
