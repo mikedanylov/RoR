d9472a38909ad9fb4832889c2a2c35a93dfb5e3f
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-18 14:11:41 +0200

    Implemented sessions#create method

--- ActiveRecord/NestedResources-Security-Paging/todolist/app/controllers/sessions_controller.rb
+++ ActiveRecord/NestedResources-Security-Paging/todolist/app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:user][:username])
+    password = params[:user][:password]
+    if user && user.authenticate(password)
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      redirect_to login_path, alert: "Invalid username or password"
+    end
   end
 
   def destroy
