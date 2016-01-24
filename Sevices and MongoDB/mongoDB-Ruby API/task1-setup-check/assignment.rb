d430ccc0966de39b5e835cc4cbba717d08aef967
Mykhailo Danylov <mike.danylov@gmail.com>
2016-01-24 12:14:29 +0200

    Done 1 taks

--- Sevices and MongoDB/mongoDB-Ruby API/task1-setup-check/assignment.rb
+++ Sevices and MongoDB/mongoDB-Ruby API/task1-setup-check/assignment.rb
@@ -1,4 +1,5 @@
 require 'pp'
+require 'mongo'
 
 class Solution
   @@db = nil
@@ -8,20 +9,22 @@ class Solution
     #create a `Mongo::Client` connection to the server using a URL (.e.g., 'mongodb://localhost:27017')
     #configure the client to use the `test` database
     #assign the client to @@db instance variable and return that client
+    db = Mongo::Client.new('mongodb://localhost:27017')
+    @@db = db.use('test')
     
   end
 
   #Implement a class method in the `Solution` class called `collection` that will
   def self.collection
     #return the `zips` collection
-    
+    @@db['zips']
   end
 
   #Implement an instance method in the `Solution` class called `sample` that will
   def sample
     #return a single document from the `zips` collection from the database. 
     #This does not have to be random. It can be first, last, or any other document in the collection.
-
+    @@db['zips'].find.first
   end
 end
 
