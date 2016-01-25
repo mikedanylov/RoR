860fb8cc99830d3a022c1e955019177a45685457
Mykhailo Danylov <mike.danylov@gmail.com>
2016-01-25 23:36:33 +0200

    Constructor for Racer model

--- Sevices and MongoDB/mongoDB-Ruby API/raceday/app/models/racer.rb
+++ Sevices and MongoDB/mongoDB-Ruby API/raceday/app/models/racer.rb
@@ -2,7 +2,8 @@
 require 'pp'
 
 class Racer
-	
+	attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
 	MONGO_URL = 'mongodb://localhost:27017'
 	MONGO_DATABASE = 'raceday_development'
 	RACE_COLLECTION = 'racers'
@@ -20,6 +21,16 @@ class Racer
   def self.all(prototype={}, sort={number:1}, skip=0, limit=nil) 
   	return @coll.find(prototype).sort(sort).skip(skip) if limit == nil
   	@coll.find(prototype).sort(sort).skip(skip).limit(limit)
+	end
+
+	def initialize(params={}) 
+	  @id=params[:_id].nil? ? params[:id] : params[:_id].to_s
+	  @number=params[:number].to_i
+	  @first_name=params[:first_name]
+	  @last_name=params[:last_name]
+	  @gender=params[:gender]
+	  @group=params[:group]
+	  @secs=params[:secs].to_i
 	end
 
 end
