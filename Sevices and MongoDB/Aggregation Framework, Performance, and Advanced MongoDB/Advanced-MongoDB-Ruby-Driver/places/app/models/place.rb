49d6b4a1e7227de5bc00f4194cd064c08c34f0a0
Mykhailo Danylov <mike.danylov@gmail.com>
2016-01-31 20:08:36 +0200

    Place class initializer takes a hash

--- Sevices and MongoDB/Aggregation Framework, Performance, and Advanced MongoDB/Advanced-MongoDB-Ruby-Driver/places/app/models/place.rb
+++ Sevices and MongoDB/Aggregation Framework, Performance, and Advanced MongoDB/Advanced-MongoDB-Ruby-Driver/places/app/models/place.rb
@@ -1,5 +1,7 @@
 class Place
 	include Mongoid::Document
+	attr_accessor :id, :formatted_address, :location, :address_components
+
 	MONGO_URL='mongodb://localhost:27017'
 	MONGO_DATABASE='test'
 	RACE_COLLECTION='race1'
@@ -19,6 +21,18 @@ class Place
 	def self.load_all file_path
 		coll = JSON.parse(File.read(file_path))
 		self.collection.insert_many(coll)
+	end
+
+	def initialize hash
+		@id = hash[:_id].to_s if hash.key?(:_id)
+		@address_components = []
+		if hash.key?(:address_components)
+			for address in hash[:address_components]
+				@address_components.push(AddressComponent.new(address))
+			end
+		end 
+		@formatted_address = hash[:formatted_address] if hash.key?(:formatted_address)
+		@location = hash[:geometry][:geolocation] if hash.key?(:geometry)
 	end
 
 end
