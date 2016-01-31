e0fc73c42edaf1b89c31e1fc4392d0697ba1be2c
Mykhailo Danylov <mike.danylov@gmail.com>
2016-01-31 19:02:08 +0200

    Place model generated

---
+++ Sevices and MongoDB/Aggregation Framework, Performance, and Advanced MongoDB/Advanced-MongoDB-Ruby-Driver/places/app/models/place.rb
@@ -0,0 +1,18 @@
+class Place
+	include Mongoid::Document
+	MONGO_URL='mongodb://localhost:27017'
+	MONGO_DATABASE='test'
+	RACE_COLLECTION='race1'
+	
+	def self.mongo_client
+		url = ENV['MONGO_URL'] ||= MONGO_URL
+		database = ENV['MONGO_DATABASE'] ||= MONGO_DATABASE
+		db = Mongo::Client.new(url)
+		@@db = db.use(database)
+	end
+
+	def self.collection
+		collection = ENV['RACE_COLLECTION'] ||= RACE_COLLECTION
+		return mongo_client[collection]
+	end
+end
