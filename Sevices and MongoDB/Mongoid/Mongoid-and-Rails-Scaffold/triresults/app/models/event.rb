017d907581cf3965a04f9101c25ecc2d9c26334b
Mykhailo Danylov <mike.danylov@gmail.com>
2016-02-22 23:40:29 +0200

    Add Event validations

--- Sevices and MongoDB/Mongoid/Mongoid-and-Rails-Scaffold/triresults/app/models/event.rb
+++ Sevices and MongoDB/Mongoid/Mongoid-and-Rails-Scaffold/triresults/app/models/event.rb
@@ -7,6 +7,8 @@ class Event
     field :u, as: :units, type: String
 
     embedded_in :parent, polymorphic: true, touch: true
+    validates :order, presence: true
+    validates :name, presence: true
 
     def meters
         case self.units
