6ff9310ecd5ab79760b69c3c8831a98abfb5c125
Mykhailo Danylov <mike.danylov@gmail.com>
2016-02-28 18:26:50 +0200

    Add next_bib method to Race model

--- Sevices and MongoDB/Mongoid/Mongoid-and-Rails-Scaffold/triresults/app/models/race.rb
+++ Sevices and MongoDB/Mongoid/Mongoid-and-Rails-Scaffold/triresults/app/models/race.rb
@@ -5,6 +5,7 @@ class Race
     field :n, as: :name, type: String
     field :date, as: :date, type: Date
     field :loc, as: :location, type: Address
+    field :next_bib, as: :next_bib, type: Integer, default: 0
 
     embeds_many :events, as: :parent, order: [:order.asc], class_name: 'Event'
     has_many :entrants, foreign_key: "race._id", dependent: :delete, order: [:secs.asc, :bib.asc]
@@ -69,6 +70,10 @@ class Race
 
     def swim_units
         swim.units
+    end
+
+    def next_bib
+        self[:next_bib] = self.inc(next_bib: 1)[:next_bib]
     end
 
 end
