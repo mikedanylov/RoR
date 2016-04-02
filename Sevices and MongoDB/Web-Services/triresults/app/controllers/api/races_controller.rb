950b5208fb545b337a3a5726b24ac40fd6099217
Mykhailo Danylov <mike.danylov@gmail.com>
2016-04-02 17:53:16 +0300

    Test responses for custom URIs

--- Sevices and MongoDB/Web-Services/triresults/app/controllers/api/races_controller.rb
+++ Sevices and MongoDB/Web-Services/triresults/app/controllers/api/races_controller.rb
@@ -1,10 +1,25 @@
 module Api
     class RacesController < ApplicationController
         def index
+            if !request.accept || request.accept == "*/*"
+                render plain: "/api/races, offset=[#{params[:offset]}], limit=[#{params[:limit]}]"
+            else
+                #real implementation ...
+            end
         end
         def show
+            if !request.accept || request.accept == "*/*"
+                render plain: "/api/races/#{params[:id]}"
+            else
+                #real implementation ...
+            end
         end
         def create
+            if !request.accept || request.accept == "*/*"
+                render plain: "#{params[:race][:name]}", status: :ok
+            else
+                #real implementation
+            end
         end
         def update
         end
