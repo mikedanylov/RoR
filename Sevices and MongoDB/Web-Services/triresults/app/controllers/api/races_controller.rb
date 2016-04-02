7dbacbb309796caefc8970ab862f5733121e241a
Mykhailo Danylov <mike.danylov@gmail.com>
2016-04-02 17:43:23 +0300

    Setup custom URIs and routes

---
+++ Sevices and MongoDB/Web-Services/triresults/app/controllers/api/races_controller.rb
@@ -0,0 +1,14 @@
+module Api
+    class RacesController < ApplicationController
+        def index
+        end
+        def show
+        end
+        def create
+        end
+        def update
+        end
+        def destroy
+        end
+    end
+end
---
+++ Sevices and MongoDB/Web-Services/triresults/app/controllers/api/results_controller.rb
@@ -0,0 +1,10 @@
+module Api
+    class ResultsController < ApplicationController
+        def index
+        end
+        def show
+        end
+        def update
+        end
+    end
+end
